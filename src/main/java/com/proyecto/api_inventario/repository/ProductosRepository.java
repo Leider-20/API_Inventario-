e25ee06f8b7266d98198e7fc9389070c64942ce0
LeiderF <leider.caicedo@udea.edu.co>
2024-07-19 02:19:38 -0500

    feat: creates and modifies repository interfaces in the "repository" package

--- src/main/java/com/proyecto/api_inventario/repository/ProductosRepository.java
+++ src/main/java/com/proyecto/api_inventario/repository/ProductosRepository.java
@@ -1,7 +1,7 @@
 package com.proyecto.api_inventario.repository;
 
-import com.proyecto.api_inventario.models.Productos;
+import com.proyecto.api_inventario.models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductosRepository extends JpaRepository<Productos, Long> {
+public interface ProductosRepository extends JpaRepository<Producto, Long> {
 }

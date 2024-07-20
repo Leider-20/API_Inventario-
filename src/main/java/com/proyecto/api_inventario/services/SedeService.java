04e4b62de5ac4da55d63990ccef1510bf511f76f
LeiderF <leider.caicedo@udea.edu.co>
2024-07-20 03:14:18 -0500

    feat: modify the service classes

--- src/main/java/com/proyecto/api_inventario/services/SedeService.java
+++ src/main/java/com/proyecto/api_inventario/services/SedeService.java
@@ -3,10 +3,12 @@ package com.proyecto.api_inventario.services;
 import com.proyecto.api_inventario.models.Sede;
 import com.proyecto.api_inventario.repository.SedeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class SedeService {
 
     @Autowired

ca93a9d3b78a31385b2627e2c0149e61dd9f418d
LeiderF <leider.caicedo@udea.edu.co>
2024-07-20 03:14:18 -0500

    feat: modify the service classes

--- src/main/java/com/proyecto/api_inventario/services/ProductosService.java
+++ src/main/java/com/proyecto/api_inventario/services/ProductosService.java
@@ -3,10 +3,12 @@ package com.proyecto.api_inventario.services;
 import com.proyecto.api_inventario.models.Producto;
 import com.proyecto.api_inventario.repository.ProductosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductosService {
 
     @Autowired

f93d349410d88a722de3a752be41a867b525fb58
LeiderF <leider.caicedo@udea.edu.co>
2024-08-03 19:08:40 -0500

    feat: modify WebConfig class

--- src/main/java/com/proyecto/api_inventario/config/WebConfig.java
+++ src/main/java/com/proyecto/api_inventario/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.1.101:5501")
+                .allowedOrigins("http://192.168.0.21:5501")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
